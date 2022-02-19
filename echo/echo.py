aaa842c1661c67c7f97c5eed13e9aaf40e558216
Vera Aguilera Puerto <gradientvera@outlook.com>
2022-02-19 14:56:36 +0100

    Escape emoji in echo raw command.

--- echo/echo.py
+++ echo/echo.py
@@ -1,4 +1,4 @@
-import discord
+import discord, re
 from discord.channel import TextChannel
 from discord.message import Message
 from discord.utils import escape_markdown, escape_mentions
@@ -64,6 +64,24 @@ class Echo(commands.Cog):
             await ctx.reply("I didn't send that message!")
             return
 
+        # Regular expression for emoji.
+        re_emoji = re.compile(
+            "["
+            "\U0001F1E0-\U0001F1FF"
+            "\U0001F300-\U0001F5FF"
+            "\U0001F600-\U0001F64F"
+            "\U0001F680-\U0001F6FF"
+            "\U0001F700-\U0001F77F"
+            "\U0001F780-\U0001F7FF"
+            "\U0001F800-\U0001F8FF"
+            "\U0001F900-\U0001F9FF"
+            "\U0001FA00-\U0001FA6F"
+            "\U0001FA70-\U0001FAFF"
+            "\U00002702-\U000027B0"
+            "\U000024C2-\U0001F251" 
+            "]+", flags=re.UNICODE
+        )
+
         contents = escape_markdown(message.content)\
             .replace("<@!", "<\\@")\
             .replace("<@&", "<\\@&")\
@@ -71,6 +89,11 @@ class Echo(commands.Cog):
             .replace("<:", "\\<:")\
             .replace("@here", "\\@here")\
             .replace("@everyone", "\\@everyone")
+
+        for emoji in re_emoji.findall(contents):
+            contents = contents.replace(emoji, f"\\{emoji}")
+
+        print(contents)
 
         await ctx.send(contents)
         await ctx.tick()
